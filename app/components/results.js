c1288116717c7a4c7ea8c2e8a82f7982ae12f014
Walter <eschenbach.walter@gmail.com>
2020-04-08 15:05:44 -0500

    first commit

---
+++ app/components/results.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { battle } from "../utils/api";
+
+class Results extends React.Component {
+  componentDidMount() {
+    const { playerOne, playerTwo } = this.props;
+    battle([playerOne, playerTwo]).then(players => {
+      console.log("data:", players);
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        Results
+        <pre>{JSON.stringify(this.props, null, 2)}</pre>
+      </div>
+    );
+  }
+}
+
+export default Results;
---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Popular from "./components/popular";
+import Battle from "./components/battle";
+
+// Component
+// State
+// Lifecycle
+// UI
+
+class App extends React.Component {
+  // JSX is converted to React.createElement by babel
+  render() {
+    return (
+      <div className="container">
+        <Battle />
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("app"));
