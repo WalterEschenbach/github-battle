b5fe6b7567420d2d0b0142358a5c57318c2e004d
Walter <eschenbach.walter@gmail.com>
2020-04-12 17:52:06 -0500

    code split

--- app/index.js
+++ app/index.js
@@ -1,26 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Popular from "./components/popular";
-import Battle from "./components/battle";
-import Results from "./components/results";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/loading";
+
+const Popular = React.lazy(() => import("./components/Popular"));
+const Battle = React.lazy(() => import("./components/Battle"));
+const Results = React.lazy(() => import("./components/Results"));
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      theme: "light",
-      toggleTheme: () => {
-        this.setState(({ theme }) => ({
-          theme: theme === "light" ? "dark" : "light",
-        }));
-      },
-    };
-  }
+  state = {
+    theme: "light",
+    toggleTheme: () => {
+      this.setState(({ theme }) => ({
+        theme: theme === "light" ? "dark" : "light",
+      }));
+    },
+  };
 
   render() {
     return (
@@ -29,12 +27,14 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path="/" component={Popular} />
+                  <Route exact path="/battle" component={Battle} />
+                  <Route path="/battle/results" component={Results} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
