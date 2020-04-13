bba490280ffaa1bdcae44220b59495ae4e0ceaeb
Walter <eschenbach.walter@gmail.com>
2020-04-12 20:06:53 -0500

    updated webpack.config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: "./app/index.js",
@@ -9,19 +8,17 @@ module.exports = {
     filename: "index_bundle.js",
     publicPath: "/",
   },
-
   module: {
     rules: [
       { test: /\.(js)$/, use: "babel-loader" },
       { test: /\.css$/, use: ["style-loader", "css-loader"] },
     ],
   },
-  mode: "production",
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
   plugins: [
     new HtmlWebpackPlugin({
       template: "app/index.html",
     }),
-    new CopyPlugin([{ from: "_redirects" }]),
   ],
   devServer: {
     historyApiFallback: true,
