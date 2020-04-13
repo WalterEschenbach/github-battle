95433d9d87c5c9928de3816c5f7f19000847932d
Walter <eschenbach.walter@gmail.com>
2020-04-12 21:39:56 -0500

    update index.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import Popular from "./components/popular";
-import Battle from "./components/battle";
-import Results from "./components/results";
 import { ThemeProvider } from "./contexts/theme";
 import Nav from "./components/nav";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Loading from "./components/loading";
+
+const Popular = React.lazy(() => import("./components/popular"));
+const Battle = React.lazy(() => import("./components/battle"));
+const Results = React.lazy(() => import("./components/results"));
 
 class App extends React.Component {
   state = {
@@ -17,6 +19,7 @@ class App extends React.Component {
       }));
     },
   };
+
   render() {
     return (
       <Router>
@@ -24,13 +27,14 @@ class App extends React.Component {
           <div className={this.state.theme}>
             <div className="container">
               <Nav />
-
-              <Switch>
-                <Route exact path="/" component={Popular} />
-                <Route exact path="/battle" component={Battle} />
-                <Route path="/battle/results" component={Results} />
-                <Route render={() => <h1>404</h1>} />
-              </Switch>
+              <React.Suspense fallback={<Loading />}>
+                <Switch>
+                  <Route exact path="/" component={Popular} />
+                  <Route exact path="/battle" component={Battle} />
+                  <Route path="/battle/results" component={Results} />
+                  <Route render={() => <h1>404</h1>} />
+                </Switch>
+              </React.Suspense>
             </div>
           </div>
         </ThemeProvider>
